20eae76d7889008b1e9a8ea7b2c649ede77a6eb8
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-01 19:01:15 -0300

    feat: add sellerName field to SaleMinDTO

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -9,7 +9,8 @@ public class SaleMinDTO {
 	private Long id;
 	private Double amount;
 	private LocalDate date;
-	
+	private String sellerName;
+
 	public SaleMinDTO(Long id, Double amount, LocalDate date) {
 		this.id = id;
 		this.amount = amount;
@@ -20,6 +21,7 @@ public class SaleMinDTO {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		sellerName = entity.getSeller().getName();
 	}
 
 	public Long getId() {
@@ -32,5 +34,9 @@ public class SaleMinDTO {
 
 	public LocalDate getDate() {
 		return date;
+	}
+
+	public String getSellerName() {
+		return sellerName;
 	}
 }
--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
 
 }
