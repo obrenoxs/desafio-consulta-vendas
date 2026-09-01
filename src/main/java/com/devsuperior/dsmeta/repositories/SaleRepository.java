d086ecea74e4aa5b5a0e02480cf37265a34797fa
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-01 19:32:15 -0300

    feat: implement JPQL query for sales report with date and name filters

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -2,10 +2,22 @@ package com.devsuperior.dsmeta.repositories;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    Page<SaleMinDTO> search1();
+    @Query(
+            "SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(s.id, s.amount, s.date, seller.name) "
+            + "FROM Sale s JOIN s.seller seller "
+            + "WHERE s.date BETWEEN :minDate AND :maxDate "
+            + "AND UPPER(seller.name) LIKE UPPER(CONCAT('%', :name, '%'))"
+    )
+    Page<SaleMinDTO> searchSales(@Param("minDate") LocalDate minDate, @Param("maxDate") LocalDate maxDate, @Param("name") String name, Pageable pageable);
 }
