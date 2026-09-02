2b9ebe3c0b980f657be39965031a32d9634c590c
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-02 08:08:02 -0300

    feat: create SaleSummaryDTO for sales summary by seller

---
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleSummaryDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsmeta.dto;
+
+public class SaleSummaryDTO {
+
+    private String sellerName;
+    private Double total;
+
+    public SaleSummaryDTO() {
+    }
+
+    public SaleSummaryDTO(String sellerName, Double total) {
+        this.sellerName = sellerName;
+        this.total = total;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+}
